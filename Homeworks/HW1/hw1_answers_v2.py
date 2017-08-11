ca4d8f99eba544996ccf4aa1d81fb68d56e0f4fe
pcunhasilva <patricksilva27@gmail.com>
2017-08-11 18:17:02 -0500

    Upload a new version of hw1_answers_v2
    few lines of code.
    add a new function.
    add bonds to the example.

--- Homeworks/HW1/hw1_answers_v2.py
+++ Homeworks/HW1/hw1_answers_v2.py
@@ -7,18 +7,29 @@ class Portfolio():
     Attributes:
         cash: A float representing the total of cash available.
         stock: A dictionary representing each type of stocks.
+               The first element of the dictionary represets the
+               total amount of shares in the portfolio and the
+               second represets the sales price per share.
         mutual funds: A dictionary representing each mutual fund.
+               The first element of the dictionary represets the
+               total amount of shares in the portfolio and the
+               second represets the sales price per share.
+        bond: A dictionary representing each bond.
+               The first element of the dictionary represets the
+               total amount of shares in the portfolio and the
+               second represets the sales price per share.
         hist: A list representing each action in the portfolio.
     """
 
     def __init__(self, cash = 0.00):
         self.cash = float(cash)
         self.stock = {} # to store stocks
-        self.mutualFund = {} # to store Mutual Funds
+        self.mutualFund = {} # to store mutual Funds
         self.bond = {} # to store bonds
         self.hist = []
 
     def history(self):
+        """Print the portfolio's log."""
         print self.hist
 
     def addCash(self, amount):
@@ -40,122 +51,95 @@ class Portfolio():
 
         new_asset = {investment.name : [float(share), \
         float(investment.sale_price)]}
-        if investment.investment_type() == 'MutualFund':
+        if investment.investment_type() == 'mutualFund':
             self.mutualFund.update(new_asset)
-        elif investment.investment_type() == 'Stock':
+        elif investment.investment_type() == 'stock':
             self.stock.update(new_asset)
         else:
             self.bond.update(new_asset)
 
     def buyInvestment(self, share, investment):
+        """Add an asset to the portfolio"""
+
         if self.cash < (share * investment.purchase_price):
             raise RuntimeError('Not enough cash available')
+
+        if investment.investment_type() == 'stock' and \
+        (share - int(share)) != 0:
+            raise RuntimeError ('Stocks can only be bought as whole units')
+
         name = investment.name
-        if investment.investment_type() == 'MutualFund':
-            self.addInvestment(share, investment)
-            self.withdrawCash(share)
-        elif investment.investment_type() == 'Stock':
-            if (share - int(share)) != 0:
-                print 'Only whole units can be bought'
-            self.addInvestment(share, investment)
-            self.withdrawCash(share * investment.purchase_price)
-        else:
-            self.addInvestment(share, investment)
-            self.withdrawCash(share * investment.purchase_price)
+        self.addInvestment(share, investment)
+        self.withdrawCash(share * investment.purchase_price)
+
         if share in (1, 1.0):
             self.hist.extend([str(1) + " share of " + name + " purchased"])
         else:
             self.hist.extend([str(share) + " shares of " + name + " purchased"])
 
     def remInvestment(self, share, name, investmenttype):
-        """ Remove an investment from the portfolio"""
-        if investmenttype == 'MutualFund':
-            if self.mutualFund[name][0] == share:
-                del self.mutualFund[name]
-            else:
-                self.mutualFund[name][0] = self.mutualFund[name][0] - share
-        elif investmenttype == 'Stock':
-            if self.stock[name][0] == share:
-                del self.stock[name]
-            else:
-                self.stock[name][0] = self.stock[name][0] - share
+        """ Remove an asset from the portfolio"""
+
+        if getattr(self, investmenttype)[name][0] == share:
+            del getattr(self, investmenttype)[name]
         else:
-            if self.bond[name][0] == share:
-                del self.bond[name]
-            else:
-                self.bond[name][0] = self.bond[name][0] - share
+            getattr(self, investmenttype)[name][0] = \
+            getattr(self, investmenttype)[name][0] - share
+
+
+    def InvestmentUnique(self, name):
+        """ Find if two items of different types of assets
+        have the same name """
+        investtype = []
+        if name in self.stock: investtype.append("stock")
+        if name in self.mutualFund: investtype.append("mutualFund")
+        if name in self.bond: investtype.append("bond")
+        return investtype
 
     def sellInvestment(self, share, name):
-        # Find if there are more than one investment with
-        # the same name in different cathegories:
-        find = name in self.stock
-        find += name in self.mutualFund
-        find += name in self.bond
+        """ Sell an assets in a portfolio """
 
-        if find == 0:
-            print 'Investment does not exist.'
+        investtype = self.InvestmentUnique(name)
 
-        elif find == 1:
-            if name in self.mutualFund:
-                if share > self.mutualFund[name][0]:
-                    print 'Not enought shares available'
-                self.remInvestment(share, name, "MutualFund")
-                price = self.mutualFund[name][1]
-                self.addCash(round(share * price, 2))
-            elif name in self.stock:
-                if share > self.stock[name][0]:
-                    print 'Not enought shares available'
-                if (share - int(share)) != 0:
-                    print 'Only units can be bought'
-                self.remInvestment(share, name, "Stock")
-                price = self.stock[name][1]
-                self.addCash(round(share * price, 2))
-            else:
-                if share > self.bond[name][0]:
-                    print 'Not enought shares available'
-                self.remInvestment(share, name, "Bond")
-                price = self.bond[name][1]
-                self.addCash(round(share * price, 2))
+        if len(investtype) == 0:
+             raise RuntimeError ('Investment does not exist.')
+        elif len(investtype) == 1:
 
-        elif find > 1:
+            if share > getattr(self, investtype[0]):
+                raise RuntimeError ('Not enought shares available')
+
+            self.remInvestment(share, name, investtype[0])
+            price = getattr(self, investtype[0])[name][1]
+            self.addCash(round(share * price, 2))
+        else:
             print "There is more than one investment with this name.",
-            print "Please, inform the type of investment:"
+            print "Please, inform the type of investment %s" % investtype
             investtype = raw_input("> ")
-            if investtype in ('Mutual Fund', 'MutualFund',
-            'mutualfund', 'mutual fund'):
-                if share > self.mutualFund[name][0]:
-                    print 'Not enought shares available'
-                self.remInvestment(share, name, "MutualFund")
-                price = self.mutualFund[name][1]
-                self.addCash(round(share * price, 2))
-            elif investtype in ('Stock', 'stock', 'Stocks', 'stocks'):
-                if share > self.stock[name][0]:
-                    print 'Not enought shares available'
-                if (share - int(share)) != 0:
-                    print 'Only whole units can be sold'
-                self.remInvestment(share, name, "Stock")
-                price = self.stock[name][1]
-                self.addCash(round(share * price, 2))
-            elif investtype in ('Bond', 'bond', 'Bonds', 'bonds'):
-                if share > self.bond[name][0]:
-                    print 'Not enought shares available'
-                self.remInvestment(share, name, "Bond")
-                price = self.bond[name][1]
-                self.addCash(round(share * price, 2))
-            else:
-                print "Type of investment does not exist."
+
+            if share > getattr(self, investtype):
+                raise RuntimeError ('Not enought shares available')
+
+            self.remInvestment(share, name, investtype)
+            price = getattr(self, investtype)[name][1]
+            self.addCash(round(share * price, 2))
 
     def __str__(self):
-        stocksstring = "Stocks: "
+        """ Define the print method to Portfolio() """
+
+        stocksstring = "stocks: "
         for key,value in self.stock.items():
             stocksstring += ('\n' + key + " " + str(value[0]))
-        mfstring = "\nMutual Fund: "
+
+        mfstring = "\nmutual Fund: "
         for key,value in self.mutualFund.items():
             mfstring += ('\n' + key + " " + str(value[0]))
+
         bondstring = "\nBonds: "
         for key,value in self.bond.items():
             bondstring += ('\n' + key + " " + str(value[0]))
+
         cashstring = "Cash: \n" + "$" + str(round(self.cash, 2)) + "\n"
+
         return cashstring + stocksstring  + mfstring + bondstring
 
 
@@ -177,58 +161,55 @@ class Investment(object):
     def __init__(self, name, purchase_price = None):
         self.name = name
 
-        if self.investment_type() == 'MutualFund':
+        if self.investment_type() == 'mutualFund':
             self.purchase_price = 1
             self.sale_price = round(uniform(0.9, 1.2), 2)
 
-        elif self.investment_type() == 'Stock':
+        else:
             if purchase_price <= 0 or purchase_price == None:
                 raise RuntimeError('Negative purchase price or invalid price.')
             self.purchase_price = purchase_price
-            self.sale_price = round(uniform(0.5 * purchase_price, \
-            1.5 * purchase_price), 2)
-
-        else:# This part is used to generate other products, as Bonds.
-            if purchase_price <= 0 or purchase_price == None:
-                raise RuntimeError('Negative purchase price or invalid price.')
-            self.purchase_price = purchase_price
-            self.sale_price = round(uniform(1 ** purchase_price, \
-            1.5 ** purchase_price), 2)
+            if self.investment_type() == 'stock':
+                self.sale_price = round(uniform(0.5 * purchase_price, \
+                1.5 * purchase_price), 2)
+            else:# This part is used to generate other products, as Bonds.
+                self.sale_price = round(uniform(1 ** purchase_price, \
+                1.5 ** purchase_price), 2)
 
     @abstractmethod
     def investment_type(self):
         """Return a string representing the type of investiment this is."""
         pass
 
-# Generate Class MutualFund
-class MutualFund(Investment):
+# Generate Class mutualFund
+class mutualFund(Investment):
 
     def __str__(self):
         return self.name
 
     def investment_type(self):
         """ Return a string representing the type of investiment this is."""
-        return 'MutualFund'
+        return 'mutualFund'
 
-# Generate Class Stock
-class Stock(Investment):
+# Generate Class stock
+class stock(Investment):
 
     def __str__(self):
         return self.name
 
     def investment_type(self):
         """ Return a string representing the type of investiment this is."""
-        return 'Stock'
+        return 'stock'
 
 # Generate Class Bond
-class Bond(Investment):
+class bond(Investment):
 
     def __str__(self):
         return self.name
 
     def investment_type(self):
         """ Return a string representing the type of investiment this is."""
-        return 'Bond'
+        return 'bond'
 
 # Test the functions using the examples in the HW.
 # Set a seed to always have the same values
@@ -237,14 +218,14 @@ print "Creates a new portfolio"
 portfolio =  Portfolio()
 print "Adds cash to the portfolio"
 portfolio.addCash(300.50)
-print "Create a Stock with price 20"
-s = Stock("HFH", 20)
+print "Create a stock with price 20"
+s = stock("HFH", 20)
 print "Buys 5 share of stock s"
 portfolio.buyInvestment(5, s)
 print "Creates MF with symbol 'BRT'"
-mf1 = MutualFund("BRT")
+mf1 = mutualFund("BRT")
 print "Creates MF with symbol 'GHT'"
-mf2 = MutualFund("GHT")
+mf2 = mutualFund("GHT")
 print "Buys 10.3 shares of 'BRT'"
 portfolio.buyInvestment(10.3, mf1)
 print "Buys 2 shares of 'GHT'"
@@ -252,7 +233,7 @@ portfolio.buyInvestment(2, mf2)
 print "Prints portfolio"
 print portfolio
 print "Sells 3 shares of 'BRT'"
-portfolio.sellInvestment(3, "BRT")
+print portfolio.sellInvestment(3, "BRT")
 print "Sells 1 share of HFH"
 portfolio.sellInvestment(1, "HFH")
 print "Removes $ 50"
@@ -260,6 +241,15 @@ portfolio.withdrawCash(50)
 print "Prints a list of all transactions"
 portfolio.history()
 print portfolio
+print "Create a bond named 'BRT'"
+b1 = bond("BRT", 3)
+print "Buys 10.3 shares of 'BRT' (bond)"
+portfolio.buyInvestment(10.3, b1)
+print "Sells 1 shares of 'BRT' (bond)"
+print portfolio.sellInvestment(1, "BRT")
+portfolio.history()
+print portfolio
+
 
 
 
@@ -267,21 +257,21 @@ print portfolio
 #
 # portfolio = Portfolio()
 # portfolio.addCash(5000)
-# s = MutualFund("s", 3)
+# s = mutualFund("s", 3)
 # print s.sale_price
 # print s.purchase_price
 # print s.investment_type()
 # portfolio.buyInvestment(50.0, s)
-# f = Stock("f", 3)
+# f = stock("f", 3)
 # print f.sale_price
 # print f.purchase_price
 # print f.investment_type()
 # portfolio.buyInvestment(50.0, f)
-# d = Bond("d", 30)
+# d = bond("d", 30)
 # print d.sale_price
 # print d.purchase_price
 # print d.investment_type()
-# dd = Stock("d", 50)
+# dd = stock("d", 50)
 # portfolio.buyInvestment(5.0, dd)
 # portfolio.buyInvestment(5.0, d)
 # print "Prints portfolio"
