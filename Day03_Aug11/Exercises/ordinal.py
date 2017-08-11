2b10e0bcf6176cab413d9bfed3228f2b50d82345
pcunhasilva <patricksilva27@gmail.com>
2017-08-11 11:33:54 -0500

    Upload ordinal with answers

--- Day03_Aug11/Exercises/ordinal.py
+++ Day03_Aug11/Exercises/ordinal.py
@@ -3,6 +3,24 @@
 #Write a test for your function
 
 def myfunction(i):
+	if i % 1 != 0:
+		raise "This is not an integer"
+	if len(str(i)) == 1:
+		if i == 1:
+			i = str(i)+"st"
+		elif i == 2:
+			i = str(i)+"nd"
+		elif i == 3:
+			i = str(i)+"rd"
+		else:
+			i = str(i)+"th"
+	else:
+		if str(i)[-1] == "1":
+			i = str(i)+"st"
+		elif str(i)[-1] == "2":
+			i = str(i)+"nd"
+		elif str(i)[-1] == "3":
+			i = str(i)+"rd"
+		else:
+			i = str(i)+"th"
 	return i
-
-print myfunction(1)	
