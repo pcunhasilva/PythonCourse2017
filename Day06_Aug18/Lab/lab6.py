b4beb24bfdb367af6da5bec61957e89b20804c23
pcunhasilva <patricksilva27@gmail.com>
2017-08-18 11:27:58 -0500

    Upload gcd function

--- Day06_Aug18/Lab/lab6.py
+++ Day06_Aug18/Lab/lab6.py
@@ -1,5 +1,10 @@
 #Exercise 1
 #Write a function to calculate the greatest common divisor of two numbers
+def gcd(num1, num2):
+    remainder = max(num1, num2) % min(num1, num2)
+    if remainder == 0:
+        return min(num1, num2)
+    return gcd(min(num1, num2), remainder)
 
 #Exercise 2
 #Write a function that returns prime numbers less than 121
@@ -7,6 +12,3 @@
 #Exercise 3
 #Write a function that gives a solution to Tower of Hanoi game
 #https://www.mathsisfun.com/games/towerofhanoi.html
-
-
-
