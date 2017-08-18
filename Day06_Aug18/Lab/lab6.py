05b8a987e395de41732a980dd6716e26b0de500b
pcunhasilva <patricksilva27@gmail.com>
2017-08-18 12:42:14 -0500

    Upload a new version of the two first functions

--- Day06_Aug18/Lab/lab6.py
+++ Day06_Aug18/Lab/lab6.py
@@ -1,10 +1,8 @@
 #Exercise 1
 #Write a function to calculate the greatest common divisor of two numbers
 def gcd(num1, num2):
-    remainder = max(num1, num2) % min(num1, num2)
-    if remainder == 0:
-        return min(num1, num2)
-    return gcd(min(num1, num2), remainder)
+    if max(num1, num2) % min(num1, num2) == 0: return min(num1, num2)
+    return gcd(min(num1, num2), max(num1, num2) % min(num1, num2))
 
 #Exercise 2
 #Write a function that returns prime numbers less than 121
@@ -18,14 +16,22 @@ def findprime(num):
         i += 1
     if remainder.count(0) < 3:
         print num
-    return primeless(num - 1)
+    return findprime(num - 1)
 
 
-findprime(121)
-
-
-
-
+# def findprime(num):
+#     if num == 1:
+#         return None
+#     i = 1
+#     remainder = []
+#     while i <= num:
+#         remainder.append(num % i)
+#         i += 1
+#     if remainder.count(0) < 3:
+#         return num
+#     prime = []
+#     prime.append(findprime(num - 1))
+#     return prime
 
 #Exercise 3
 #Write a function that gives a solution to Tower of Hanoi game
