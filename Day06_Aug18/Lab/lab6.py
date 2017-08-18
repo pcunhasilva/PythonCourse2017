ce05cbe73b959acdf054f1071a53faf89d083be0
pcunhasilva <patricksilva27@gmail.com>
2017-08-18 13:31:45 -0500

    upload a version of findprime that stores the values

--- Day06_Aug18/Lab/lab6.py
+++ Day06_Aug18/Lab/lab6.py
@@ -18,20 +18,26 @@ def findprime(num):
         print num
     return findprime(num - 1)
 
+def findprime2(num):
+    if 'prime' not in globals():
+        global prime
+        prime = []
+    if num == 1:
+        result = prime
+        del globals()['prime']
+        return result
+    i = 1
+    remainder = []
+    while i <= num:
+        remainder.append(num % i)
+        i += 1
+    if remainder.count(0) < 3:
+        prime.append(num - 1)
+    return findprime2(num - 1)
 
-# def findprime(num):
-#     if num == 1:
-#         return None
-#     i = 1
-#     remainder = []
-#     while i <= num:
-#         remainder.append(num % i)
-#         i += 1
-#     if remainder.count(0) < 3:
-#         return num
-#     prime = []
-#     prime.append(findprime(num - 1))
-#     return prime
+
+# findprime(121)
+print findprime2(121)
 
 #Exercise 3
 #Write a function that gives a solution to Tower of Hanoi game
