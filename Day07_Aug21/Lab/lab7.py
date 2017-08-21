e7ec35be8210f2d0772fbcf9f0e288215203b348
pcunhasilva <patricksilva27@gmail.com>
2017-08-21 18:59:32 -0500

    Upload answers to lab 7
    Upload all but the Eulerian tour. The code for findAllPaths() still has
    some limitations.

--- Day07_Aug21/Lab/lab7.py
+++ Day07_Aug21/Lab/lab7.py
@@ -8,12 +8,12 @@ def makeLink(G, node1, node2):
   if node2 not in G:
     G[node2] = {}
   (G[node2])[node1] = 1
-  return G 
+  return G
 
 # Ring Network
-ring = {} # empty graph 
+ring = {} # empty graph
 
-n = 5 # number of nodes 
+n = 5 # number of nodes
 
 # Add in edges
 for i in range(n):
@@ -23,21 +23,49 @@ for i in range(n):
 print len(ring)
 
 # How many edges?
-print sum([len(ring[node]) for node in ring.keys()])/2 
+print sum([len(ring[node]) for node in ring.keys()])/2
 
 
 # Grid Network
-# TODO: create a square graph with 256 nodes and count the edges 
+# TODO: create a square graph with 256 nodes and count the edges
+
+# It creates the square graph with the 256 nodes and diagonals.
+square = {}
+for i in range(1, 17):
+    for j in range(0, 226, 16):
+        d = i + j
+        square = makeLink(square, d , d + 16)
+        if d not in range(16, 257, 16):
+            square = makeLink(square, d , d + 1)
+            square = makeLink(square, d , d + 17)
+        if d not in range(1, 242, 16):
+            square = makeLink(square, d , d + 15)
+        if d == 240:
+            for k in range(1, 17):
+                if d + k != 256:
+                    square = makeLink(square, d + k, d + k + 1)
+
 # TODO: define a function countEdges
+def countEdges(graph):
+    f = graph
+    k = []
+    for i in f.keys():
+        for j in f[i].keys():
+            k.append([i, j])
+    for i in range(0, len(k)):
+        k[i].sort()
+    unique = set(map(tuple, k))
+    return len(unique)
+
 
 
 # Social Network
 class Actor(object):
   def __init__(self, name):
-    self.name = name 
+    self.name = name
 
   def __repr__(self):
-    return self.name 
+    return self.name
 
 ss = Actor("Susan Sarandon")
 jr = Actor("Julia Roberts")
@@ -61,31 +89,32 @@ makeLink(movies, ah, ms) # Devil Wears Prada
 makeLink(movies, ah, jr) # Valentine's Day
 
 
-
 # How many nodes in movies?
+len(movies)
 # How many edges in movies?
+countEdges(movies)
 
 def tour(graph, nodes):
   for i in range(len(nodes)):
-    node = nodes[i] 
+    node = nodes[i]
     if node in graph.keys():
-      print node 
+      print node
     else:
       print "Node not found!"
-      break 
+      break
     if i+1 < len(nodes):
       next_node = nodes[i+1]
       if next_node in graph.keys():
         if next_node in graph[node].keys():
-          pass 
+          pass
         else:
           print "Can't get there from here!"
-          break 
+          break
 
-# TODO: find an Eulerian tour of the movie network and check it 
+# TODO: find an Eulerian tour of the movie network and check it
 tour(movies, [ah,ms])
 tour(movies, [rd,ms,kb,jr,ah,ms])
-movie_tour = [] 
+movie_tour = []
 tour(movies, movie_tour)
 
 
@@ -106,24 +135,51 @@ print findPath(movies, jr, ms)
 
 # TODO: implement findShortestPath()
 # print findShortestPath(movies, ms, ss)
+def findShortestPath(graph, start, end, path = []):
+    x = findAllPaths(graph, start, end, path)
+    lengs = [len(i) for i in x]
+    return x[lengs.index(min(lengs))]
 
 # TODO: implement findAllPaths() to find all paths between two nodes
+
+# This function is capable to find all paths from the start, and all
+# different paths from the second node. However, it is not capable to find
+# all possible paths if the third node has more than one possible path.
+def findAllPaths(graph, start, end, path = []):
+    if 'paths' not in globals():
+        global paths
+        paths = []
+    if end in movies[start]: paths.append(path + [start] + [end])
+    for node in movies[start]:
+        path = []
+        path = path + [start]
+        for subnode in movies[node]:
+            if subnode != start:
+                    path = path + [node]
+                    paths.append(findPath(movies, subnode, end, path))
+    paths = filter(None, paths)
+    for i in range(0, len(paths)):
+        paths[i] = pd.Series(paths[i]).drop_duplicates().tolist()
+    result = paths
+    del globals()['paths']
+    return result
+
 # allPaths = findAllPaths(movies, jr, ms)
 # for path in allPaths:
 #   print path
 
 # Copyright (c) 2014 Matt Dickenson
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
