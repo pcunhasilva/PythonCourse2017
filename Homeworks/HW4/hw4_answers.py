9b28cab7b1de66defaaed1163346d0db972cf4ec
pcunhasilva <patricksilva27@gmail.com>
2017-08-22 16:41:24 -0500

    Upload a new version of counting()
    Now counting() can deal with negative numbers.

--- Homeworks/HW4/hw4_answers.py
+++ Homeworks/HW4/hw4_answers.py
@@ -8,53 +8,19 @@ def selection(nlist):
 
 def counting(nlist):
     unsortedlist = nlist
-    # Part 1
     indexarray = range(min(unsortedlist), max(unsortedlist) + 1)
     countarray = [unsortedlist.count(i) for i in indexarray]
-    # Part 2
-    newcount = []
+    newcount = [None] * len(countarray)
     n = 0
+    total = 0
     while n < len(countarray):
-        if n == 0:
-            newcount.append(countarray[n])
-        elif n == len(countarray):
-            newcount.append(countarray[n - 1])
-        else:
-            newcount.append(newcount[n - 1] + countarray[n])
+        newcount[n] = total
+        d = countarray[n]
+        total += countarray[n]
         n += 1
     # Part 3
     sortedlist = [None] * len(unsortedlist)
-    for i in range(len(unsortedlist)):
-        if i >= len(unsortedlist): continue
-        if unsortedlist[i] in sortedlist:
-            sortedlist[newcount[unsortedlist[i] - 1] - 2] = unsortedlist[i]
-        else:
-            sortedlist[newcount[unsortedlist[i] - 1] - 1] = unsortedlist[i]
+    for i in unsortedlist:
+        sortedlist[newcount[indexarray.index(i)]] = i
+        newcount[indexarray.index(i)] += 1
     return sortedlist
-
-
-# def counting(nlist):
-#     unsortedlist = nlist
-#     # Part 1
-#     indexarray = range(min(unsortedlist), max(unsortedlist) + 1)
-#     countarray = [unsortedlist.count(i) for i in indexarray]
-#     # Part 2
-#     newcount = []
-#     n = 0
-#     while n < len(countarray):
-#         if n == 0:
-#             newcount.append(countarray[n])
-#         elif n == len(countarray):
-#             newcount.append(countarray[n - 1])
-#         else:
-#             newcount.append(newcount[n - 1] + countarray[n])
-#         n += 1
-#     # Part 3
-#     sortedlist = [None] * len(unsortedlist)
-#     for i in range(len(unsortedlist)):
-#         if i >= len(unsortedlist): continue
-#         if unsortedlist[i] in sortedlist:
-#             sortedlist[newcount[unsortedlist[i] - 1] - 2] = unsortedlist[i]
-#         else:
-#             sortedlist[newcount[unsortedlist[i] - 1] - 1] = unsortedlist[i]
-#     return sortedlist
