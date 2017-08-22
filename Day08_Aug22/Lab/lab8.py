7bc8e7ff8b91fbc2bfb3db0356fe48bc82eae45d
pcunhasilva <patricksilva27@gmail.com>
2017-08-22 13:37:57 -0500

    Upload the first to tasks

--- Day08_Aug22/Lab/lab8.py
+++ Day08_Aug22/Lab/lab8.py
@@ -11,15 +11,22 @@ keyword = re.compile(r"the ")
 # search file for keyword, line by line
 for line in text:
   if keyword.search(line):
-    print line 
+    print line
 
 # TODO: print all lines that DO NOT contain "the "
+for line in text:
+    if not keyword.search(line):
+        print line
+
 # TODO: print lines that contain a word of any length starting with s and ending with e
-  
+allwords = re.findall(r"\bs\w*e\b", " ".join(str(x) for x in text))
+for line in text:
+    for i in range(len(allwords)):
+        if str(allwords[i]) in line:
+            print line
+
 # date = raw_input("Please enter a date in the format MM.DD.YY: ")
 # Print the date input in the following format:
 # Month: MM
 # Day: DD
 # Year: YY
-
-
