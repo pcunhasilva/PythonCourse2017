8a7db259ff1bede9db1b3473beddf377973d7806
pcunhasilva <patricksilva27@gmail.com>
2017-08-22 15:39:05 -0500

    Upload a first version of counting and some test
    This version of counting only works with non-negative numbers

--- Homeworks/HW4/hw4_answers.py
+++ Homeworks/HW4/hw4_answers.py
@@ -3,6 +3,58 @@ def selection(nlist):
     sortedlist = []
     while len(startedlist) > 0:
         sortedlist.append(min(startedlist))
-        del startedlist[sortedlist.index(sortedlist[-1])]
+        del startedlist[startedlist.index(sortedlist[-1])]
+    return sortedlist
 
-test = [2, 3, 1, 6, 2]
+def counting(nlist):
+    unsortedlist = nlist
+    # Part 1
+    indexarray = range(min(unsortedlist), max(unsortedlist) + 1)
+    countarray = [unsortedlist.count(i) for i in indexarray]
+    # Part 2
+    newcount = []
+    n = 0
+    while n < len(countarray):
+        if n == 0:
+            newcount.append(countarray[n])
+        elif n == len(countarray):
+            newcount.append(countarray[n - 1])
+        else:
+            newcount.append(newcount[n - 1] + countarray[n])
+        n += 1
+    # Part 3
+    sortedlist = [None] * len(unsortedlist)
+    for i in range(len(unsortedlist)):
+        if i >= len(unsortedlist): continue
+        if unsortedlist[i] in sortedlist:
+            sortedlist[newcount[unsortedlist[i] - 1] - 2] = unsortedlist[i]
+        else:
+            sortedlist[newcount[unsortedlist[i] - 1] - 1] = unsortedlist[i]
+    return sortedlist
+
+
+# def counting(nlist):
+#     unsortedlist = nlist
+#     # Part 1
+#     indexarray = range(min(unsortedlist), max(unsortedlist) + 1)
+#     countarray = [unsortedlist.count(i) for i in indexarray]
+#     # Part 2
+#     newcount = []
+#     n = 0
+#     while n < len(countarray):
+#         if n == 0:
+#             newcount.append(countarray[n])
+#         elif n == len(countarray):
+#             newcount.append(countarray[n - 1])
+#         else:
+#             newcount.append(newcount[n - 1] + countarray[n])
+#         n += 1
+#     # Part 3
+#     sortedlist = [None] * len(unsortedlist)
+#     for i in range(len(unsortedlist)):
+#         if i >= len(unsortedlist): continue
+#         if unsortedlist[i] in sortedlist:
+#             sortedlist[newcount[unsortedlist[i] - 1] - 2] = unsortedlist[i]
+#         else:
+#             sortedlist[newcount[unsortedlist[i] - 1] - 1] = unsortedlist[i]
+#     return sortedlist
---
+++ Homeworks/HW4/hw4_answers_test.py
@@ -0,0 +1,26 @@
+import unittest
+import random
+import hw4_answers
+
+class SortTest(unittest.TestCase):
+
+    def setUp(self):
+        self.test1 = [1, 2, 2, 40, 0, 3, 5, 5]
+        self.sortedtest1 = sorted(self.test1)
+        self.test2 = [-1, -5, -10, -2, 0, 3]
+        self.sortedtest2 = sorted(self.test2)
+
+    # Test selection:
+    def test_positive_selection(self):
+        self.assertEqual(hw4_answers.selection(self.test1), self.sortedtest1)
+    def test_positive_negative_selection(self):
+        self.assertEqual(hw4_answers.selection(self.test2), self.sortedtest2)
+
+    # Test selection:
+    def test_positive_counting(self):
+        self.assertEqual(hw4_answers.counting(self.test1), self.sortedtest1)
+    def test_positive_negative_counting(self):
+        self.assertEqual(hw4_answers.counting(self.test2), self.sortedtest2)
+
+if __name__ == '__main__':
+  unittest.main()
